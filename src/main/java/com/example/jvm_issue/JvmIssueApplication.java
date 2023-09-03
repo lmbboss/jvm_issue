d65ff1574bb62e0f95d8318d27c1be450b86eb72
栾铭斌 <luanmingbin@byteplan.com>
2023-09-03 19:36:47 +0800

    init project

---
+++ src/main/java/com/example/jvm_issue/JvmIssueApplication.java
@@ -0,0 +1,13 @@
+package com.example.jvm_issue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmIssueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvmIssueApplication.class, args);
+    }
+
+}
